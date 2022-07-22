9c47edc507784e944bd5d422f925ffaf3b92e78e
postreldi <postreldi@gmail.com>
2022-07-22 17:27:55 +0300

    add test commit

--- amajig.js
+++ amajig.js
@@ -1,26 +1,32 @@
-function clunk(times) {
- var num = times;
- while (num > 0) {
- display("clunk");
- num = num - 1;
- }
+function clunk(times) 
+{
+  var num = times;
+  while (num > 0) 
+    {
+      display("clunk");
+      num = num - 1;
+    }
 }
-function thingamajig(size) {
+function thingamajig(size) 
+{
  var facky = 1;
  clunkCounter = 0;
- if (size == 0) {
- display("clank");
+ if (size == 0) 
+ {
+  display("clank");
  } else if (size == 1) {
- display("thunk");
+  display("thunk");
  } else {
- while (size > 1) {
- facky = facky * size;
- size = size - 1;
- }
- clunk(facky);
+  while (size > 1) 
+  {
+    facky = facky * size;
+    size = size - 1;
+  }
+  clunk(facky);
  }
 }
-function display(output) {
+function display(output) 
+{
  console.log(output);
  clunkCounter = clunkCounter + 1;
 }
